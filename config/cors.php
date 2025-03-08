303b795cb7c627fe1ee7a1e41504ba2c42e04da3
Josué Aquino <aquinojosue889@gmail.com>
2025-03-08 16:01:40 -0300

    W

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie' , 'storage/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie' , 'storage/*', 'update'],
 
     'allowed_methods' => ['*'],
 
